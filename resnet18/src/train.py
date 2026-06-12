770a462e6ab3ae4b2951a5aedddd81ef65ff030b
Tuan Anh <letuananh04062006@gmail.com>
2026-06-12 23:48:15 +0700

    feat: implement Feature Pyramid Network (FPN) module

--- resnet18/src/train.py
+++ resnet18/src/train.py
@@ -6,15 +6,11 @@ from PIL import Image
 from torchvision import transforms, models
 
 class ResNet18(nn.Module):
-    def __init__(self, num_classes, num_anchors):
+    def __init__(self):
         super().__init__()
-        self.num_anchors = num_anchors
-        self.num_classes = num_classes
-        self.element_per_anchor = 4 + num_classes 
-        out_channels = self.num_anchors * self.element_per_anchor  # Tổng số kênh (channels) đầu ra tại mỗi ô lưới
-        
+
         self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels = 3, out_channels = 64, kernel_size = 7, stride = 2, padding = 3),
+            nn.Conv2d(in_channels = 3, out_channels = 64, kernel_size = 7, stride = 2, padding = 3, bias = False),
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size = 3, stride = 2, padding = 1)
@@ -22,154 +18,190 @@ class ResNet18(nn.Module):
 
         self.relu = nn.ReLU()
         
-    # CỤM 1 (conv2_x): Biến đổi từ 64 kênh (112x112) -> 128 kênh (56x56)
+    # CỤM 1 (conv2_x): Biến đổi từ 64 kênh (112x112) -> 64 kênh (56x56)
         self.conv2_x_block1 = nn.Sequential(
-            nn.Conv2d(in_channels = 64, out_channels = 64, kernel_size = 3, stride = 1, padding = 1),
+            nn.Conv2d(in_channels = 64, out_channels = 64, kernel_size = 3, stride = 1, padding = 1, bias = False),
             nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.Conv2d(in_channels = 64, out_channels = 64, kernel_size = 3, stride = 1, padding = 1),
+            nn.Conv2d(in_channels = 64, out_channels = 64, kernel_size = 3, stride = 1, padding = 1, bias = False),
             nn.BatchNorm2d(64)
         )
         self.conv2_x_block2 = nn.Sequential(
-            nn.Conv2d(in_channels = 64, out_channels = 64, kernel_size = 3, stride = 1, padding = 1),
+            nn.Conv2d(in_channels = 64, out_channels = 64, kernel_size = 3, stride = 1, padding = 1, bias = False),
             nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.Conv2d(in_channels = 64, out_channels = 64, kernel_size = 3, stride = 1, padding = 1),
+            nn.Conv2d(in_channels = 64, out_channels = 64, kernel_size = 3, stride = 1, padding = 1, bias = False),
             nn.BatchNorm2d(64)
         )
         
     # CỤM 2 (conv3_x): Biến đổi từ 64 kênh (56x56) -> 128 kênh (28x28)
         self.conv3_x_block1 = nn.Sequential(
-            nn.Conv2d(in_channels = 64, out_channels = 128, kernel_size = 3, stride = 2, padding = 1),
+            nn.Conv2d(in_channels = 64, out_channels = 128, kernel_size = 3, stride = 2, padding = 1, bias = False),
             nn.BatchNorm2d(128),
             nn.ReLU(),
-            nn.Conv2d(in_channels = 128, out_channels = 128, kernel_size = 3, stride = 1, padding = 1),
+            nn.Conv2d(in_channels = 128, out_channels = 128, kernel_size = 3, stride = 1, padding = 1, bias = False),
             nn.BatchNorm2d(128),
         )
         self.conv3_x_block2 = nn.Sequential(
-            nn.Conv2d(in_channels = 128, out_channels = 128, kernel_size = 3, stride = 1, padding = 1),
+            nn.Conv2d(in_channels = 128, out_channels = 128, kernel_size = 3, stride = 1, padding = 1, bias = False),
             nn.BatchNorm2d(128),
             nn.ReLU(),
-            nn.Conv2d(in_channels = 128, out_channels = 128, kernel_size = 3, stride = 1, padding = 1),
+            nn.Conv2d(in_channels = 128, out_channels = 128, kernel_size = 3, stride = 1, padding = 1, bias = False),
             nn.BatchNorm2d(128)
         )
         self.downsample3 = nn.Sequential(
-            nn.Conv2d(in_channels = 64, out_channels = 128, kernel_size = 1, stride = 2),
+            nn.Conv2d(in_channels = 64, out_channels = 128, kernel_size = 1, stride = 2, bias = False),
             nn.BatchNorm2d(128)
         )
 
         
     # CỤM 3 (conv4_x): Biến đổi từ 128 kênh (28x28) -> 256 kênh (14x14)  
         self.conv4_x_block1 = nn.Sequential(
-            nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1),
+            nn.Conv2d(in_channels = 128, out_channels = 256, kernel_size = 3, stride = 2, padding = 1, bias = False),
             nn.BatchNorm2d(256),
             nn.ReLU(),
-            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels = 256, out_channels = 256, kernel_size = 3, stride = 1, padding = 1, bias = False),
             nn.BatchNorm2d(256)
         )
         self.conv4_x_block2 = nn.Sequential(
-            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels = 256, out_channels = 256, kernel_size = 3, stride = 1, padding = 1, bias = False),
             nn.BatchNorm2d(256),
             nn.ReLU(),
-            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels = 256, out_channels = 256, kernel_size = 3, stride = 1, padding = 1, bias = False),
             nn.BatchNorm2d(256)
         )
         self.downsample4 = nn.Sequential(
-            nn.Conv2d(128, 256, kernel_size=1, stride=2),
+            nn.Conv2d(in_channels = 128, out_channels = 256, kernel_size = 1, stride = 2, bias = False),
             nn.BatchNorm2d(256)
         )
 
         
     # CỤM 4 (conv5_x): Biến đổi từ 256 kênh (14x14) -> 512 kênh (7x7)
         self.conv5_x_block1 = nn.Sequential(
-            nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1),
+            nn.Conv2d(in_channels = 256, out_channels = 512, kernel_size = 3, stride = 2, padding = 1, bias = False),
             nn.BatchNorm2d(512),
             nn.ReLU(),
-            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels = 512, out_channels = 512, kernel_size = 3, stride = 1, padding = 1, bias = False),
             nn.BatchNorm2d(512)
         )
         self.conv5_x_block2 = nn.Sequential(
-            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels = 512, out_channels = 512, kernel_size = 3, stride = 1, padding = 1, bias = False),
             nn.BatchNorm2d(512),
             nn.ReLU(),
-            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels = 512, out_channels = 512, kernel_size = 3, stride = 1, padding = 1, bias = False),
             nn.BatchNorm2d(512)
         )
         self.downsample5 = nn.Sequential(
-            nn.Conv2d(256, 512, kernel_size=1, stride=2),
+            nn.Conv2d(in_channels = 256, out_channels = 512, kernel_size = 1, stride = 2, bias = False),
             nn.BatchNorm2d(512)
         )
 
-        # ĐẦU DỰ ĐOÁN CHUẨN: Dùng Conv 1x1 thay thế hoàn toàn cho FC mạng tuyến tính
-        self.prediction = nn.Sequential(
-            nn.Conv2d(in_channels = 512, out_channels = 256, kernel_size = 3, padding = 1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=256, out_channels = out_channels, kernel_size = 1) # Conv 1x1 quyết định số đầu ra
-        )
-        
-        
     def forward(self, x):
-        x = self.conv1(x)
+        x1 = self.conv1(x)
 
     # CỤM 1
         # KHỐI 1
-        identity = x                     # Nhánh phụ: Giữ nguyên bản sao của x làm đường tắt
-        fx = self.conv2_x_block1(x)      # Nhánh chính: Tính toán qua khối 1
-        x = self.relu(fx + identity)     # Cộng khối dư và kích hoạt ReLU -> Đây là đầu ra khối 1
+        identity = x1                     # Nhánh phụ: Giữ nguyên bản sao của x làm đường tắt
+        fx = self.conv2_x_block1(x1)      # Nhánh chính: Tính toán qua khối 1
+        x2 = self.relu(fx + identity)     # Cộng khối dư và kích hoạt ReLU -> Đây là đầu ra khối 1
         
         # KHỐI 2 
-        identity = x                     # Nhánh phụ: Giữ nguyên đầu vào của khối 2
-        fx = self.conv2_x_block2(x)      # Nhánh chính: Tính toán qua khối 2 với trọng số riêng
-        x = self.relu(fx + identity)     # Cộng khối dư và kích hoạt ReLU -> Đầu ra tầng conv2_x
+        identity = x2                    # Nhánh phụ: Giữ nguyên đầu vào của khối 2
+        fx = self.conv2_x_block2(x2)      # Nhánh chính: Tính toán qua khối 2 với trọng số riêng
+        c2 = self.relu(fx + identity)     # Cộng khối dư và kích hoạt ReLU -> Đầu ra tầng conv2_x
         
     # CỤM 2
         # KHỐI 1
-        identity = self.downsample3(x)
-        fx = self.conv3_x_block1(x)
-        x = self.relu(fx + identity)
+        identity = self.downsample3(c2)
+        fx = self.conv3_x_block1(c2)
+        x3 = self.relu(fx + identity)
 
         # KHỐI 2
-        identity = x
-        fx = self.conv3_x_block2(x)
-        x = self.relu(fx + identity)
+        identity = x3
+        fx = self.conv3_x_block2(x3)
+        c3 = self.relu(fx + identity)
 
         
     # CỤM 3 (conv4_x)
         # KHỐI 1
-        identity = self.downsample4(x)
-        fx = self.conv4_x_block1(x)
-        x = self.relu(fx + identity)
+        identity = self.downsample4(c3)
+        fx = self.conv4_x_block1(c3)
+        x4 = self.relu(fx + identity)
 
         # KHỐI 2
-        identity = x
-        fx = self.conv4_x_block2(x)
-        x = self.relu(fx + identity)
+        identity = x4
+        fx = self.conv4_x_block2(x4)
+        c4 = self.relu(fx + identity)
         
     # CỤM 4 (conv5_x)
         # KHỐI 1
-        identity = self.downsample5(x)
-        fx = self.conv5_x_block1(x)
-        x = self.relu(fx + identity)
+        identity = self.downsample5(c4)
+        fx = self.conv5_x_block1(c4)
+        x5 = self.relu(fx + identity)
 
         # KHỐI 2
-        identity = x
-        fx = self.conv5_x_block2(x)
-        features = self.relu(fx + identity)
+        identity = x5
+        fx = self.conv5_x_block2(x5)
+        c5 = self.relu(fx + identity)
+
+        return {
+            "c2": c2,
+            "c3": c3,
+            "c4": c4,
+            "c5": c5
+        }
 
 
-        predictions = self.prediction(features)   # Kích thước hiện tại: [Batch_size, num_anchors * (4 + num_classes), H_grid, W_grid]
+class FPN(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
 
-        batch_size, _, h, w = predictions.size()
-        predictions = predictions.permute(0, 2, 3, 1).contiguous() 
-        # Kích thước sau permute: [Batch_size, H_grid, W_grid, num_anchors * element_per_anchor]
+        self.lat2 = nn.Conv2d(in_channels[0], out_channels, kernel_size = 1)
+        self.lat3 = nn.Conv2d(in_channels[1], out_channels, kernel_size = 1)
+        self.lat4 = nn.Conv2d(in_channels[2], out_channels, kernel_size = 1)
+        self.lat5 = nn.Conv2d(in_channels[3], out_channels, kernel_size = 1)
+
+        self.fpn_conv2 = nn.Conv2d(out_channels, out_channels, kernel_size = 3, padding = 1)
+        self.fpn_conv3 = nn.Conv2d(out_channels, out_channels, kernel_size = 3, padding = 1)
+        self.fpn_conv4 = nn.Conv2d(out_channels, out_channels, kernel_size = 3, padding = 1)
+        self.fpn_conv5 = nn.Conv2d(out_channels, out_channels, kernel_size = 3, padding = 1)
         
-        predictions = torch.flatten(predictions, start_dim=1)
-        # Kích thước đầu ra cuối cùng: [Batch_size, 7 * 7 * num_anchors * (4 + num_classes)]
+        self.upsample = nn.Upsample(scale_factor = 2,  mode= 'nearest')
 
-        return predictions
-    
+    def forward(self, inputs):
+        c2, c3, c4, c5 = inputs["c2"], inputs["c3"], inputs["c4"], inputs["c5"]
+        
+        lat5 = self.lat5(c5)
+        lat4 = self.lat4(c4)
+        lat3 = self.lat3(c3)
+        lat2 = self.lat2(c2)
 
-ResNet = ResNet18(num_classes = 20, num_anchors = 2)
+        p5 = lat5
+        p4 = self.upsample(p5) + lat4
+        p3 = self.upsample(p4) + lat3
+        p2 = self.upsample(p3) + lat2
+
+        p5 = self.fpn_conv5(p5)
+        p4 = self.fpn_conv4(p4)
+        p3 = self.fpn_conv3(p3)
+        p2 = self.fpn_conv2(p2)
+
+        return {
+            "p2": p2,
+            "p3": p3,
+            "p4": p4,
+            "p5": p5
+        }
+           
+
+in_channels = [64, 128, 256, 512]
+
+print(len(in_channels))
+
+resnet = ResNet18()
+fpn = FPN(in_channels, 256)
+
 test = torch.randn(1, 3, 224, 224)
-ResNet(test)
+test = resnet(test)
+test = fpn(test)
+print(test["p2"].shape)
